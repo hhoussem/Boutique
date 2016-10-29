c847d158af2dccb2c24432ba2cee189b5048e83d
hhoussem <houssemeddine.hamrouni@gmail.com>
2016-10-29 20:21:18 +0200

    first commit dependecy

---
+++ application/src/main/java/fr/alma/soa/boutique/application/App.java
@@ -0,0 +1,13 @@
+package fr.alma.soa.boutique.application;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
